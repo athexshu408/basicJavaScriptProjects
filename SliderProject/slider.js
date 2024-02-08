c344579bf27bdd3f57a9b9553a1c115e8a61074b
athexshu408 <143508858+athexshu408@users.noreply.github.com>
2024-02-08 10:59:38 +0530

    Add files via upload

---
+++ SliderProject/slider.js
@@ -0,0 +1,72 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
+
+let slideNumber = 1;
+
+const length = images.length;
+
+for(let i=0; i<length; i++) {
+    const div = document.createElement("div");
+    div.className="button";
+    bottom.appendChild(div);
+   
+   }
+   
+   const buttons = document.querySelectorAll(".button")
+   buttons[0].style.backgroundColor = "white"
+   
+   const resetBg  = ()=>{
+       buttons.forEach((button)=>{
+           button.style.backgroundColor = "transparent"
+       });
+   
+   }
+   
+   buttons.forEach((button,i)=>{
+       button.addEventListener("click", () => {
+           resetBg()
+       slider.style.transform = `translateX(-${i * 600}px`
+       slideNumber = i + 1;
+       button.style.backgroundColor = "white"
+       }
+   )})
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 600}px)`;
+  slideNumber++;
+};
+const prevSlide = () => {
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 600}px)`;
+    slideNumber--;
+  };
+const fristImage = () => {
+  slider.style.transform = `translateX(0px)`;
+  slideNumber = 1;
+};
+
+
+const getLastImage =()=>{
+    slider.style.transform = `translateX(-${(length -1) * 600}px)`;
+    slideNumber = length;
+}
+
+const changeColor = ()=>{
+    resetBg ();
+    buttons[slideNumber-1].style.backgroundColor="white";
+}
+
+right.addEventListener("click", () => {
+  console.log("You  clicked right arrow");
+  slideNumber < images.length ? nextSlide() : fristImage();
+  changeColor();
+});
+
+left.addEventListener("click", () => {
+  console.log("You  clicked left arrow");
+  slideNumber > 1 ? prevSlide () : getLastImage ();
+  changeColor ();
+
+});
